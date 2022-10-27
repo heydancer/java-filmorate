06192d6dc8e6a752853a82966691e0f3feb02262
Alexandr Ustiantcev <a.ustiantcev@gmail.com>
2022-10-28 00:52:58 +0300

    feat: added Film class

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,28 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+
+    @NotNull(message = "Id cannot be null")
+    private int id;
+
+    @NotNull(message = "Name cannot be null")
+    @NotBlank(message = "Name cannot be empty")
+    private String name;
+
+    @NotNull(message = "Description cannot be null")
+    @Size(max = 200, message = "Length of the description should not exceed 200 characters")
+    private String description;
+
+    @NotNull(message = "Release Date cannot be null")
+    private LocalDate releaseDate;
+
+    @NotNull(message = "Duration cannot be null")
+    @Min(value = 1, message = "Duration should be positive")
+    private Integer duration;
+}
